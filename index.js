d68647118754e268f0e91ee7e54611462db8afe9
Filipe Tavares <fwmtavares@gmail.com>
2019-03-15 03:41:56 -0300

    Authentication with Google OAuth

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express')
+const passportConfig = require('./services/passport')
 
 const app = express()
 
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-    res.send({ hello: 'World' })
-})
+require('./routes/authRoutes')(app)
 
 app.listen(port, () => {
     console.log('Servidor rodando na porta ' + port)
